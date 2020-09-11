722bff2a903162a87b1d66efd0574cd1749010ae
Robert Adamczewski <robert@adamczewski.me>
2020-09-11 19:09:30 +0200

    feat: the clear configuration testing functions

--- src/app/index.ts
+++
@@ -1,9 +0,0 @@
-console.log(`Hello from app`)
-
-export const testFunction = (): string => {
-  return 'It is my test!'
-}
-
-export const greet = (): string => {
-  return 'Hello, it is my app!'
-}
