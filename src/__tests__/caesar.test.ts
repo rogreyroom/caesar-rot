fd3066599c80537abd8ca88bbccdff650843664b
Robert Adamczewski <robert@adamczewski.me>
2020-09-11 20:14:25 +0200

    feat: add the isUpperCase test and function

--- src/__tests__/caesar.test.ts
+++ src/__tests__/caesar.test.ts
@@ -1,4 +1,4 @@
-import { convertToString, isEmpty, isAlphanumeric } from '../app/modules/caesar'
+import { convertToString, isEmpty, isAlphanumeric, isUpperCase } from '../app/modules/caesar'
 
 describe('Caesar13: create a caesar13 cipher code from the given value.', () => {
   describe('change given input to the string', () => {
@@ -50,5 +50,28 @@ describe('Caesar13: create a caesar13 cipher code from the given value.', () => 
       const input = '}'
       expect(isAlphanumeric(input)).toBe(false)
     })
+  })
+})
+
+describe('check if the given argument is an upper case', () => {
+  test('is the "A" string upper case', () => {
+    const input = 'A'
+    expect(isUpperCase(input)).toBe(true)
+  })
+  test('is the "B" string upper case', () => {
+    const input = 'B'
+    expect(isUpperCase(input)).toBe(true)
+  })
+  test('is not the "a" string upper case', () => {
+    const input = 'a'
+    expect(isUpperCase(input)).toBe(false)
+  })
+  test('is not the "j" string upper case', () => {
+    const input = 'j'
+    expect(isUpperCase(input)).toBe(false)
+  })
+  test('is not the "#" string upper case', () => {
+    const input = '1'
+    expect(isUpperCase(input)).toBe(false)
   })
 })
