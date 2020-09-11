cb57fb1983d6ab81fcc9e3cc2aefa6ef0d1af84b
Robert Adamczewski <robert@adamczewski.me>
2020-09-11 19:18:59 +0200

    feat: add the isEmpty test and function

--- src/__tests__/caesar.test.ts
+++ src/__tests__/caesar.test.ts
@@ -1,4 +1,4 @@
-import { convertToString } from '../app/modules/caesar'
+import { convertToString, isEmpty } from '../app/modules/caesar'
 
 describe('Caesar13: create a caesar13 cipher code from the given value.', () => {
   describe('change given input to the string', () => {
@@ -10,6 +10,18 @@ describe('Caesar13: create a caesar13 cipher code from the given value.', () => 
     test('returns a string value from the given 1122 string', () => {
       const input = 1122
       expect(convertToString(input)).toBe('1122')
+    })
+  })
+
+  describe('check if the given input string is empty', () => {
+    test('is empty', () => {
+      const input = ''
+      expect(isEmpty(input)).toBe(true)
+    })
+
+    test('is not empty', () => {
+      const input = 'aaa'
+      expect(isEmpty(input)).toBe(false)
     })
   })
 })
--- src/app/modules/caesar.ts
+++ src/app/modules/caesar.ts
@@ -1,3 +1,7 @@
 export const convertToString = (input: string | number): string => {
   return input.toString()
 }
+
+export const isEmpty = (input: string): boolean => {
+  return input === '' ? true : false
+}
