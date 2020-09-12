b391463e2e4dfc769d47489afde871e2e4444c1e
Robert Adamczewski <robert@adamczewski.me>
2020-09-12 19:53:23 +0200

    feat: add the caesarRot test and function

--- src/__tests__/caesar.test.ts
+++ src/__tests__/caesar.test.ts
@@ -5,9 +5,10 @@ import {
   isUpperCase,
   isOverArrayLength,
   shiftIndexValue,
+  caesarRot,
 } from '../app/modules/caesar'
 
-describe('Caesar13: create a caesar13 cipher code from the given value.', () => {
+describe('Caesar ROT: create a caesar ROT cipher code from the given value.', () => {
   describe('change given input to the string', () => {
     test('returns a string value from the given 1122mmm string', () => {
       const input = '1122mmm'
@@ -58,103 +59,155 @@ describe('Caesar13: create a caesar13 cipher code from the given value.', () => 
       expect(isAlphanumeric(input)).toBe(false)
     })
   })
-})
 
-describe('check if the given argument is an upper case', () => {
-  test('is the "A" string upper case', () => {
-    const input = 'A'
-    expect(isUpperCase(input)).toBe(true)
-  })
-  test('is the "B" string upper case', () => {
-    const input = 'B'
-    expect(isUpperCase(input)).toBe(true)
-  })
-  test('is not the "a" string upper case', () => {
-    const input = 'a'
-    expect(isUpperCase(input)).toBe(false)
-  })
-  test('is not the "j" string upper case', () => {
-    const input = 'j'
-    expect(isUpperCase(input)).toBe(false)
-  })
-  test('is not the "#" string upper case', () => {
-    const input = '1'
-    expect(isUpperCase(input)).toBe(false)
-  })
-})
-
-describe('check if the given index is bigger then array length', () => {
-  const arrayLength = 26
-  test('is index = 30 over array length with ROT13', () => {
-    const indexValue = 30
-    const rot = 13
-    expect(isOverArrayLength(indexValue, arrayLength, rot)).toBe(true)
-  })
-  test('is index = 27 over array length with ROT13', () => {
-    const indexValue = 27
-    const rot = 13
-    expect(isOverArrayLength(indexValue, arrayLength, rot)).toBe(true)
-  })
-  test('is not index = 1 over array length  with ROT13', () => {
-    const indexValue = 1
-    const rot = 13
-    expect(isOverArrayLength(indexValue, arrayLength, rot)).toBe(false)
-  })
-  test('is not index = 12 over array length with ROT13', () => {
-    const indexValue = 12
-    const rot = 13
-    expect(isOverArrayLength(indexValue, arrayLength, rot)).toBe(false)
-  })
-})
-
-describe('check if the given index was shifted by the ROT', () => {
-  const arrayLength = 26
-
-  test('change index = 0  to index = 13 with the use of ROT13', () => {
-    const indexValue = 0
-    const rot = 13
-    expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(13)
-  })
-  test('change index = 3  to index = 16 with the use of ROT13', () => {
-    const indexValue = 3
-    const rot = 13
-    expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(16)
-  })
-  test('change index = 20  to index = 7 with the use of ROT13', () => {
-    const indexValue = 20
-    const rot = 13
-    expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(7)
+  describe('check if the given argument is an upper case', () => {
+    test('is the "A" string upper case', () => {
+      const input = 'A'
+      expect(isUpperCase(input)).toBe(true)
+    })
+    test('is the "B" string upper case', () => {
+      const input = 'B'
+      expect(isUpperCase(input)).toBe(true)
+    })
+    test('is not the "a" string upper case', () => {
+      const input = 'a'
+      expect(isUpperCase(input)).toBe(false)
+    })
+    test('is not the "j" string upper case', () => {
+      const input = 'j'
+      expect(isUpperCase(input)).toBe(false)
+    })
+    test('is not the "#" string upper case', () => {
+      const input = '1'
+      expect(isUpperCase(input)).toBe(false)
+    })
   })
 
-  test('change index = 0  to index = 0 with the use of ROT0', () => {
-    const indexValue = 0
-    const rot = 0
-    expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(indexValue)
-  })
-  test('change index = 25  to index = 25 with the use of ROT0', () => {
-    const indexValue = 25
-    const rot = 0
-    expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(indexValue)
-  })
-  test('change index = 13  to index = 13 with the use of ROT0', () => {
-    const indexValue = 13
-    const rot = 0
-    expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(indexValue)
+  describe('check if the given index is bigger then array length', () => {
+    const arrayLength = 26
+    test('is index = 30 over array length with ROT13', () => {
+      const indexValue = 30
+      const rot = 13
+      expect(isOverArrayLength(indexValue, arrayLength, rot)).toBe(true)
+    })
+    test('is index = 27 over array length with ROT13', () => {
+      const indexValue = 27
+      const rot = 13
+      expect(isOverArrayLength(indexValue, arrayLength, rot)).toBe(true)
+    })
+    test('is not index = 1 over array length  with ROT13', () => {
+      const indexValue = 1
+      const rot = 13
+      expect(isOverArrayLength(indexValue, arrayLength, rot)).toBe(false)
+    })
+    test('is not index = 12 over array length with ROT13', () => {
+      const indexValue = 12
+      const rot = 13
+      expect(isOverArrayLength(indexValue, arrayLength, rot)).toBe(false)
+    })
   })
 
-  test('change index = 1  to index = 6 with the use of ROT5', () => {
-    const indexValue = 1
-    const rot = 5
-    expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(6)
+  describe('check if the given index was shifted by the ROT', () => {
+    const arrayLength = 26
+    test('change index = 0  to index = 13 with the use of ROT13', () => {
+      const indexValue = 0
+      const rot = 13
+      expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(13)
+    })
+    test('change index = 3  to index = 16 with the use of ROT13', () => {
+      const indexValue = 3
+      const rot = 13
+      expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(16)
+    })
+    test('change index = 20  to index = 7 with the use of ROT13', () => {
+      const indexValue = 20
+      const rot = 13
+      expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(7)
+    })
+    test('change index = 0  to index = 0 with the use of ROT0', () => {
+      const indexValue = 0
+      const rot = 0
+      expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(indexValue)
+    })
+    test('change index = 25  to index = 25 with the use of ROT0', () => {
+      const indexValue = 25
+      const rot = 0
+      expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(indexValue)
+    })
+    test('change index = 13  to index = 13 with the use of ROT0', () => {
+      const indexValue = 13
+      const rot = 0
+      expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(indexValue)
+    })
+    test('change index = 1  to index = 6 with the use of ROT5', () => {
+      const indexValue = 1
+      const rot = 5
+      expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(6)
+    })
+    test('change index = 10  to index = 15 with the use of ROT5', () => {
+      const indexValue = 10
+      const rot = 5
+      expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(15)
+    })
+    test('change index = 24  to index = 3 with the use of ROT5', () => {
+      const indexValue = 24
+      const rot = 5
+      expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(3)
+    })
   })
-  test('change index = 10  to index = 15 with the use of ROT5', () => {
-    const indexValue = 10
-    const rot = 5
-    expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(15)
-  })
-  test('change index = 24  to index = 3 with the use of ROT5', () => {
-    const indexValue = 24
-    const rot = 5
-    expect(shiftIndexValue(indexValue, rot, arrayLength)).toBe(3)
+
+  describe('check if the given string was ciphered by the ROT', () => {
+    // const alphabet = generateAlphabet()
+
+    test('change string "a" to "n" with the use of ROT13', () => {
+      const value = 'a'
+      const rot = 13
+      expect(caesarRot(value, rot)).toBe('n')
+    })
+    test('change string "d" to "q" with the use of ROT13', () => {
+      const value = 'd'
+      const rot = 13
+      expect(caesarRot(value, rot)).toBe('q')
+    })
+    test('change string "u" to "h" with the use of ROT13', () => {
+      const value = 'u'
+      const rot = 13
+      expect(caesarRot(value, rot)).toBe('h')
+    })
+    test('change string "a" to "a" with the use of ROT0', () => {
+      const value = 'a'
+      const rot = 0
+      expect(caesarRot(value, rot)).toBe(value)
+    })
+    test('change string "z" to "z" with the use of ROT0', () => {
+      const value = 'z'
+      const rot = 0
+      expect(caesarRot(value, rot)).toBe(value)
+    })
+    test('change string "n" to "n" with the use of ROT0', () => {
+      const value = 'n'
+      const rot = 0
+      expect(caesarRot(value, rot)).toBe(value)
+    })
+    test('change string "a" to "n" with the use of ROT5', () => {
+      const value = 'a'
+      const rot = 5
+      expect(caesarRot(value, rot)).toBe('f')
+    })
+    test('change string "b" to "q" with the use of ROT5', () => {
+      const value = 'b'
+      const rot = 5
+      expect(caesarRot(value, rot)).toBe('g')
+    })
+    test('change string "k" to "p" with the use of ROT5', () => {
+      const value = 'k'
+      const rot = 5
+      expect(caesarRot(value, rot)).toBe('p')
+    })
+    test('change string "y" to "d" with the use of ROT5', () => {
+      const value = 'y'
+      const rot = 5
+      expect(caesarRot(value, rot)).toBe('d')
+    })
   })
 })
--- src/app/modules/caesar.ts
+++ src/app/modules/caesar.ts
@@ -1,3 +1,7 @@
+import { generateAlphabet } from './generateAlphabet'
+
+const alphabet: string[] = generateAlphabet()
+
 export const convertToString = (input: string | number): string => {
   return input.toString()
 }
@@ -26,4 +30,26 @@ export const shiftIndexValue = (index: number, rot: number, arrayLength: number)
   if (isOverArrayLength(index, arrayLength, rot)) return rot - (arrayLength - index)
 
   return index + rot
+}
+
+export const caesarRot = (value: string, rot: number): string => {
+  const valueString: string = convertToString(value)
+  const valueArray: string[] = valueString.split('')
+  let resultString = ''
+
+  if (isEmpty(value)) throw new Error('Given string is empty!')
+
+  valueArray.forEach((char) => {
+    if (!isAlphanumeric) return (resultString = resultString.concat(char))
+
+    const valueIndex: number = alphabet.indexOf(char.toLowerCase())
+    const newValueIndex: number = shiftIndexValue(valueIndex, rot, alphabet.length)
+    const newChar: string = alphabet[newValueIndex]
+
+    resultString = isUpperCase(char)
+      ? resultString.concat(newChar.toUpperCase())
+      : resultString.concat(newChar)
+  })
+
+  return resultString
 }
