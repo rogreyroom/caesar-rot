43a41a7b75768cdc61797acf3a705f7ac7e2c162
Robert Adamczewski <robert@adamczewski.me>
2020-09-20 12:09:36 +0200

    feat: get document selectors, generate the select options, and add event listeners to the selectors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 import 'normalize.css'
 import './scss/style.scss'
+import * as app from './app'
 
-const message = 'The main index.ts'
+const bodyTarget = document.querySelector<HTMLBodyElement>('.body--js')!
+const rotSelect = document.querySelector<HTMLSelectElement>('.rot-select--js')!
+const inputText = document.querySelector<HTMLInputElement>('.input-text--js')!
+const outputText = document.querySelector<HTMLParagraphElement>('.output-text--js')!
+const targetElements: {
+  rot: HTMLSelectElement
+  input: HTMLInputElement
+  output: HTMLParagraphElement
+} = {
+  rot: rotSelect,
+  input: inputText,
+  output: outputText,
+}
 
-document.write(message)
-console.log(message)
+app.generateOptions(rotSelect)
+
+inputText.addEventListener('click', (): void => {
+  app.clearTextFields([inputText, outputText])
+})
+bodyTarget.addEventListener('click', ({ target }: MouseEvent) => {
+  app.runCaesar(target, null, targetElements)
+})
+inputText.addEventListener('keyup', ({ target, key }: KeyboardEvent) => {
+  app.runCaesar(target, key, targetElements)
+})
